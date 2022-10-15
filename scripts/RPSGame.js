8e3122ea63634388257519c2667cf6a2e1b6aaae
MarkoKushta <markokushta39@gmail.com>
2022-10-15 14:03:01 +0200

    Update RPSGame.js

--- scripts/RPSGame.js
+++ scripts/RPSGame.js
@@ -9,9 +9,6 @@ game(startGame);
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function selectionCheck(playerSelection){
-    
-}
 
 function game(startGame){
    
